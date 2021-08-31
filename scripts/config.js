9f6626dd82ee80918083e4fac145bf256d536d94
Daniel <68907471+ArthegaAsdweri@users.noreply.github.com>
2021-08-31 22:17:48 +0200

    switched the possible languages

--- scripts/config.js
+++ scripts/config.js
@@ -7,3 +7,23 @@ DND5E.toolProficiencies.alc = "WC5E.AlchemistsSupplies";
 //DND5E.toolProficiencies.eng = "WC5E.EngineersTools";
 DND5E.toolProficiencies.poi = "WC5E.PoisonersKit";
 //DND5E.toolProficiencies.thi = "WC5E.ThievesTools";
+
+DND5E.languages = {
+    "common"    : "WC5E.Languages.Common",
+    "dwarven"   : "WC5E.Languages.Dwarven",
+    "draenei"   : "WC5E.Languages.Draenei",
+    "draconic"  : "WC5E.Languages.Draconic",
+    "elven"     : "WC5E.Languages.Elven",
+    "eredun"    : "WC5E.Languages.Eredun",
+    "gnomish"   : "WC5E.Languages.Gnomish",
+    "goblin"    : "WC5E.Languages.Goblin",
+    "kalimag"   : "WC5E.Languages.Kalimag",
+    "lowcommon" : "WC5E.Languages.LowCommon",
+    "orcish"    : "WC5E.Languages.Orcish",
+    "ogre"      : "WC5E.Languages.Ogre",
+    "pandaren"  : "WC5E.Languages.Pandaren",
+    "shathyar"  : "WC5E.Languages.ShathYar",
+    "taurahe"   : "WC5E.Languages.Taurahe",
+    "titan"     : "WC5E.Languages.Titan",
+    "troll"     : "WC5E.Languages.Troll"
+};
