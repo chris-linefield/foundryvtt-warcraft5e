ef12e9b46c85bcc2d564b808db6421cb9a06e740
Daniel <68907471+ArthegaAsdweri@users.noreply.github.com>
2021-09-19 16:53:04 +0200

    Death Knight finished

--- scripts/config.js
+++ scripts/config.js
@@ -3,14 +3,14 @@ console.log('>> Wc5e: Adjusting 5e Config');
 import { DND5E } from "../../../systems/dnd5e/module/config.js";
 import { }       from "./config_class_features.js";
 
-DND5E.toolProficiencies = {
+/*DND5E.toolProficiencies = {
     "alc"  : "WC5E.AlchemistsSupplies",
     "disg" : "WC5E.DisguiseKit",
     "eng"  : "WC5E.EngineersTools",
     "forg" : "WC5E.ForgeryKit",
     "poi"  : "WC5E.PoisonersKit",
     "thi"  : "WC5E.ThievesTools"
-};
+};*/
 
 DND5E.languages = {
     "common"    : "WC5E.Languages.Common",
@@ -26,7 +26,7 @@ DND5E.languages = {
     "orcish"    : "WC5E.Languages.Orcish",
     "ogre"      : "WC5E.Languages.Ogre",
     "pandaren"  : "WC5E.Languages.Pandaren",
-    "shathyar"  : "WC5E.Languages.ShathYar",
+    "shathyar"  : "WC5E.Languages.ShatYar",
     "taurahe"   : "WC5E.Languages.Taurahe",
     "titan"     : "WC5E.Languages.Titan",
     "troll"     : "WC5E.Languages.Troll"
