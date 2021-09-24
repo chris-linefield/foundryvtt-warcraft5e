40cd9a0c32db271e444b682009048ef0a1a2c2ea
Daniel <68907471+ArthegaAsdweri@users.noreply.github.com>
2021-09-24 21:37:36 +0200

    Criminal and Entertainer Backgrounds

--- scripts/hook_drop.js
+++ scripts/hook_drop.js
@@ -3,8 +3,15 @@
 
 Hooks.on('preCreateItem', function(document, data, options, userId) {
     if (data.flags.wc5e && data.flags.wc5e.type === 'background') {
+
+        //Check for Variants
+        if(typeof(data.flags.wc5e.variants) !== 'undefined') {
+
+        }
+
         let targetActor = game.actors.get(document.parent.id);
         targetActor.update({['data.details.background'] : data});
+        //targetActor.addEmbeddedItems(items, true);
         return false;
     }
 });
