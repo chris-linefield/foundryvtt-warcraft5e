57028af77eb81306a3bc49cd7b196646928da075
Daniel <68907471+ArthegaAsdweri@users.noreply.github.com>
2022-02-18 17:23:38 +0100

    + Added Monster "Acolyte" + Added Spell "Light" + Added Spell "Smite" + Added Language "Necril"

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 console.log('>> Wc5e: Adjusting 5e Config');
 
-import { DND5E } from "../../../systems/dnd5e/module/config.js";
-import { }       from "./config_class_features.js";
+import {DND5E} from "../../../systems/dnd5e/module/config.js";
+import {} from "./config_class_features.js";
 
 /*DND5E.toolProficiencies = {
     "alc"  : "WC5E.AlchemistsSupplies",
@@ -17,23 +17,24 @@ DND5E.abilities.con = "WC5E.Character.Stamina";
 DND5E.abilities.wis = "WC5E.Character.Spirit";
 
 DND5E.languages = {
-    "common"    : "WC5E.Languages.Common",
-    "dwarven"   : "WC5E.Languages.Dwarven",
-    "draenei"   : "WC5E.Languages.Draenei",
-    "draconic"  : "WC5E.Languages.Draconic",
-    "elven"     : "WC5E.Languages.Elven",
-    "eredun"    : "WC5E.Languages.Eredun",
-    "gnomish"   : "WC5E.Languages.Gnomish",
-    "goblin"    : "WC5E.Languages.Goblin",
-    "kalimag"   : "WC5E.Languages.Kalimag",
-    "lowcommon" : "WC5E.Languages.LowCommon",
-    "orcish"    : "WC5E.Languages.Orcish",
-    "ogre"      : "WC5E.Languages.Ogre",
-    "pandaren"  : "WC5E.Languages.Pandaren",
-    "shathyar"  : "WC5E.Languages.ShathYar",
-    "taurahe"   : "WC5E.Languages.Taurahe",
-    "titan"     : "WC5E.Languages.Titan",
-    "troll"     : "WC5E.Languages.Troll"
+    "common": "WC5E.Languages.Common",
+    "dwarven": "WC5E.Languages.Dwarven",
+    "draenei": "WC5E.Languages.Draenei",
+    "draconic": "WC5E.Languages.Draconic",
+    "elven": "WC5E.Languages.Elven",
+    "eredun": "WC5E.Languages.Eredun",
+    "gnomish": "WC5E.Languages.Gnomish",
+    "goblin": "WC5E.Languages.Goblin",
+    "kalimag": "WC5E.Languages.Kalimag",
+    "necril": "WC5E.Languages.Necril",
+    "lowcommon": "WC5E.Languages.LowCommon",
+    "orcish": "WC5E.Languages.Orcish",
+    "ogre": "WC5E.Languages.Ogre",
+    "pandaren": "WC5E.Languages.Pandaren",
+    "shathyar": "WC5E.Languages.ShathYar",
+    "taurahe": "WC5E.Languages.Taurahe",
+    "titan": "WC5E.Languages.Titan",
+    "troll": "WC5E.Languages.Troll"
 };
 
 DND5E.damageTypes.holy = "WC5E.UI.DamageTypes.Holy";
