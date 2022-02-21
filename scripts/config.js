4940ad7b131ab866bffa62a8559d92f16cc64fc2
Daniel <68907471+ArthegaAsdweri@users.noreply.github.com>
2022-02-21 14:25:37 +0100

    + Added Chapter 5 - Treasures - Infused Items

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 console.log('>> Wc5e: Adjusting 5e Config');
 
-import { DND5E } from "../../../systems/dnd5e/module/config.js";
-import { }       from "./config_class_features.js";
+import {DND5E} from "../../../systems/dnd5e/module/config.js";
+import {} from "./config_class_features.js";
 
 /*DND5E.toolProficiencies = {
     "alc"  : "WC5E.AlchemistsSupplies",
@@ -16,22 +16,27 @@ DND5E.abilities.dex = "WC5E.Character.Agility";
 DND5E.abilities.con = "WC5E.Character.Stamina";
 DND5E.abilities.wis = "WC5E.Character.Spirit";
 
+DND5E.attunements[3] = "WC5E.UI.Attunements.GreaterAttunement";
+
+DND5E.itemRarity.veryRare = "WC5E.UI.ItemRarity.Epic";
+DND5E.itemRarity.varies = "WC5E.UI.ItemRarity.Varies";
+
 DND5E.languages = {
-    "common"    : "WC5E.Languages.Common",
-    "dwarven"   : "WC5E.Languages.Dwarven",
-    "draenei"   : "WC5E.Languages.Draenei",
-    "draconic"  : "WC5E.Languages.Draconic",
-    "elven"     : "WC5E.Languages.Elven",
-    "eredun"    : "WC5E.Languages.Eredun",
-    "gnomish"   : "WC5E.Languages.Gnomish",
-    "goblin"    : "WC5E.Languages.Goblin",
-    "kalimag"   : "WC5E.Languages.Kalimag",
-    "lowcommon" : "WC5E.Languages.LowCommon",
-    "orcish"    : "WC5E.Languages.Orcish",
-    "ogre"      : "WC5E.Languages.Ogre",
-    "pandaren"  : "WC5E.Languages.Pandaren",
-    "shathyar"  : "WC5E.Languages.ShathYar",
-    "taurahe"   : "WC5E.Languages.Taurahe",
-    "titan"     : "WC5E.Languages.Titan",
-    "troll"     : "WC5E.Languages.Troll"
+    "common": "WC5E.Languages.Common",
+    "dwarven": "WC5E.Languages.Dwarven",
+    "draenei": "WC5E.Languages.Draenei",
+    "draconic": "WC5E.Languages.Draconic",
+    "elven": "WC5E.Languages.Elven",
+    "eredun": "WC5E.Languages.Eredun",
+    "gnomish": "WC5E.Languages.Gnomish",
+    "goblin": "WC5E.Languages.Goblin",
+    "kalimag": "WC5E.Languages.Kalimag",
+    "lowcommon": "WC5E.Languages.LowCommon",
+    "orcish": "WC5E.Languages.Orcish",
+    "ogre": "WC5E.Languages.Ogre",
+    "pandaren": "WC5E.Languages.Pandaren",
+    "shathyar": "WC5E.Languages.ShathYar",
+    "taurahe": "WC5E.Languages.Taurahe",
+    "titan": "WC5E.Languages.Titan",
+    "troll": "WC5E.Languages.Troll"
 };
